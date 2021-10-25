95d74e8a3a34e594e84aed254f178a8257a35aa5
allumik <alvinmeltsov@gmail.com>
2021-10-26 00:54:51 +0300

    WSLg and W11 variables first test

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
 with lib;
 let
   defaultUser = "nixos";
+  automountPath = "/mnt";
   syschdemd = import ./syschdemd.nix { inherit lib pkgs config defaultUser; };
 in
 {
@@ -12,35 +13,71 @@ in
 
   # WSL is closer to a container than anything else
   boot.isContainer = true;
-
-  environment.etc.hosts.enable = false;
-  environment.etc."resolv.conf".enable = false;
-
   networking.dhcpcd.enable = false;
 
-  users.users.${defaultUser} = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-  };
+  users = {
+    users.${defaultUser} = {
+      # fix UID as is expected needed in wslg
+      uid = 1000;
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+    };
 
-  users.users.root = {
-    shell = "${syschdemd}/bin/syschdemd";
-    # Otherwise WSL fails to login as root with "initgroups failed 5"
-    extraGroups = [ "root" ];
+    users.root = {
+      shell = "${syschdemd}/bin/syschdemd";
+      # Otherwise WSL fails to login as root with "initgroups failed 5"
+      extraGroups = [ "root" ];
+    };
   };
 
   security.sudo.wheelNeedsPassword = false;
 
   # Disable systemd units that don't make sense on WSL
-  systemd.services."serial-getty@ttyS0".enable = false;
-  systemd.services."serial-getty@hvc0".enable = false;
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@".enable = false;
+  systemd = {
+    # Don't allow emergency mode, because we don't have a console.
+    enableEmergencyMode = false;
 
-  systemd.services.firewall.enable = false;
-  systemd.services.systemd-resolved.enable = false;
-  systemd.services.systemd-udevd.enable = false;
+    services = {
+      "serial-getty@ttyS0".enable = false;
+      "serial-getty@hvc0".enable = false;
+      "getty@tty1".enable = false;
+      "autovt@".enable = false;
 
-  # Don't allow emergency mode, because we don't have a console.
-  systemd.enableEmergencyMode = false;
+      firewall.enable = false;
+      systemd-resolved.enable = false;
+      systemd-udevd.enable = false;
+    };
+  };
+
+  environment = {
+    # WSLg support
+    etc = {
+      "wsl.conf".text = ''
+        [automount]
+        enabled=true
+        mountFsTab=true
+        root=${automountPath}/
+        options=metadata,uid=1000,gid=100
+      '';
+      hosts.enable = false;
+      "resolv.conf".enable = false;
+    };
+
+    variables = {
+      DISPLAY = ":0";
+      WAYLAND_DISPLAY = "wayland-0";
+
+      PULSE_SERVER = "${automountPath}/wslg/PulseServer";
+      XDG_RUNTIME_DIR = "${automountPath}/wslg/runtime-dir";
+      WSL_INTEROP = "/run/WSL/1_interop";
+    };
+  };
+
+  system.activationScripts.copy-launchers.text = ''
+    for x in applications icons; do
+      echo "Copying /usr/share/$x"
+      rm -rf /usr/share/$x
+      cp -r $systemConfig/sw/share/$x/. /usr/share/$x
+    done
+  '';
 }
